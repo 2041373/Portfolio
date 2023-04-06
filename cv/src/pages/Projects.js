edf7c4b8d2de108910a1d49b767065504cf2d82b
nazar zaheer <73424320+2041373@users.noreply.github.com>
2023-04-06 14:35:14 +0100

    Project Cards
    project cards

--- cv/src/pages/Projects.js
+++ cv/src/pages/Projects.js
@@ -4,27 +4,78 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import '../styles/Projects.css';
 
-export default function Projects() {
+function Projects() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <div  style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+    <Card sx={{ maxWidth: 345, margin: '1rem'}}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
+          image="https://picsum.photos/345/140"
+          alt="random"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            Sample Computer Science Project
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut
+            commodo massa. Integer maximus, mi id malesuada maximus, augue quam
+            fringilla elit, vel pretium elit elit ut risus.
           </Typography>
         </CardContent>
       </CardActionArea>
     </Card>
+
+    <Card sx={{ maxWidth: 345, margin: '1rem'}}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://picsum.photos/345/140"
+          alt="random"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Sample Computer Science Project
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut
+            commodo massa. Integer maximus, mi id malesuada maximus, augue quam
+            fringilla elit, vel pretium elit elit ut risus.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+
+    <Card sx={{ maxWidth: 345, margin: '1rem'}}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://picsum.photos/345/140"
+          alt="random"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Sample Computer Science Project
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut
+            commodo massa. Integer maximus, mi id malesuada maximus, augue quam
+            fringilla elit, vel pretium elit elit ut risus.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+
+    </div>
+    
   );
 }
+
+export default Projects;
+
